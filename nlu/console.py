48a70dbe4185941e70cf41e1e7de2f55cc56a598
Sean Bethard <sean@seanbethard.com>
2019-01-19 18:08:14 -0600

    create nlu interpreter, parse incoming queries

--- nlu/console.py
+++ nlu/console.py
@@ -1,7 +1,27 @@
+"""
+
+create nlu interpreter
+parse incoming queries
+
+$ python console.py model_20190119-124624 "recommend garage rock from 1991"
+
+"""
+
+
+import argparse
 import os
 from rasa_nlu.model import Interpreter
 
-model_dir = os.path.join()
 
-interpreter = Interpreter.load(model_dir)
-interpreter.parse(u'')
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model", help="name of nlu model")
+    parser.add_argument("query", help="input query")
+    args = parser.parse_args()
+    
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    model_dir = os.path.join(dir_path, "models", "nlu", "default", args.model)
+    interpreter = Interpreter.load(model_dir)
+    print(interpreter.parse(args.query))
+    
